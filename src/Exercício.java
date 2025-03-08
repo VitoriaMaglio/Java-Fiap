53ac2e965a225b55a7cf7783704a392ac6ad1816
Vitória Valentina Maglio <vitoriamaglio@gmail.com>
2025-03-08 13:24:30 -0300

    ddd-aula01/02

---
+++ src/Exercício.java
@@ -0,0 +1,5 @@
+public class Exercício {
+    /*/ Exercício; Faça um programa que aceite um número (multiplo de 10) e que retorne quantas vezes as cédulas de 50,20,10 são divisíveis pelo número.
+
+     */
+}
