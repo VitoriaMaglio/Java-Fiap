7d82d011b33db6495266ff6fefb6c13558d732ff
VitoriaMaglio <vitoriamaglio@gmail.com>
2025-03-14 09:50:12 -0300

    Projetos POO

---
+++ Aula_teoria.java
@@ -0,0 +1,70 @@
+public class Aula_teoria {
+    public static void main(String[] args) {
+/* Orientação a objetos: POO (1960)
+POO é Programação orientada a objetos.
+Paradigma POO : tentar trazer o mundo real para o sistema Projeto Documenta&ccedil;&atilde;o Sistema
+Programação orientada a objetos tem o objetivo de orientar o c&oacute;digo em torno de objetos do mundo real, para entender as rela&ccedil;&otilde;es entre esses objetos para tentar tornar o c&oacute;digo mais pr&oacute;ximo do mundo real.
+
+Conceitos:
++ significa public
+Abstração : extrair o que mais importa de algo, ignorando detalhes desnecess&aacute;rios. Usada para entender s.c sob a perspectiva do orientador.
+Objetos dentro do JAVA :
+Classe = representa o obj do mundo real; modelo de um objeto, genérico, monta uma classe para criar objetos
+Diagrama de classe UML(Unified Modeling Language) representa visualmente os modelos do sistema de software e o DC ajuda a visualizar como as classes se integram.
+
+         Nome(negrito)
+         -var
+         +ações(o q cada obj faz dentro da classe)
+
+Obj é a representação concreta de uma classe.
+14/03
+Variável é um espaço na memória RAM
+Quando vc cria uma classe voce está criando um tipo novo
+Vc tbm tem que declarar o objeto com a palavra new ->
+tipo    nome      criar    tipo()
+Receita bolofuba = new Receita();
+métodos -> responsável por uma ação do objeto
++ exibirReceita (): VOID
+void significa que não tem retorno o método.
+método com retorno :
+String é um método que realiza a ação e retorna uma string, e deve ter um return
+
+
+
+
+
+ */
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Tudo que você quer printar na tela voce precisa usar o comando leitor. pois você precisa que o sistemna leia o comando.
+leitor.nextInt();  para ler um número inteiro;
+leitor.nextLine();  parta ler nome composto
+
+
+
+
+
+ */
--- TesteLivro.java
+++ TesteLivro.java
@@ -3,17 +3,19 @@ public class TesteLivro {
 //classe de execução
 // Criar um objeto -> Instanciar um objeto
 //Sintaxe -> Tipo nomeObjeto = new Tipo();
+
+
              Livro meuLivro = new Livro();
              meuLivro.titulo = "A História Secreta";
              meuLivro.autor = "Donna Tartt";
-             meuLivro.editora = "";
+             meuLivro.editora = "Companhia das Letras";
              meuLivro.paginas = 502;
              meuLivro.preco = 90.00;
              meuLivro.tipoCapa = "Capa mole";
              meuLivro.resumo = "Uma história ...";
 
 
-mmm
+
 
 
 
