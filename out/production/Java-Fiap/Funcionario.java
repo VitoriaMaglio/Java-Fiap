c5dc7542ed31f09694d263016cd0ee9657d8fe01
Vitória Valentina Maglio <vitoriamaglio@gmail.com>
2025-03-20 20:03:39 -0300

    Add files via upload

---
+++ out/production/Java-Fiap/Funcionario.java
@@ -0,0 +1,26 @@
+public class Funcionario {
+    //Uma classe é um molde ou modelo para criar objetos.
+    //Um objeto é uma instância de uma classe. Ele contém valores específicos para os atributos definidos na classe.
+    /*Classe de modelagem epresenta um conceito ou entidade do mundo real
+     dentro do código, armazenando seus atributos e comportamentos.*/
+
+    /*Diagrama de classe: Funcionario
+    atributos da classe
+
+    +nome: String
+    +sobrenome: String
+    +horasTrabalhadas: int
+    +valorPorHora: double
+    +nomeCompleto(): String
+    +calcularSalario(): double
+     */
+
+    String nome;
+    String sobrenome;
+    int horasTrabalhadas;
+    double valorPorHora;
+    String nomeCompleto;
+    double calcularSalario;
+
+
+}
